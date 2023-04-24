8256d5b825019e9a2d3fda745d620517b1cbd5a5
Lara <lara.kah@icloud.com>
2023-04-24 10:39:15 +0200

    Initial commit

---
+++ src/main/java/com/example/oblig_3_webprogrammering/Oblig3WebprogrammeringApplication.java
@@ -0,0 +1,13 @@
+package com.example.oblig_3_webprogrammering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3WebprogrammeringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3WebprogrammeringApplication.class, args);
+    }
+
+}
